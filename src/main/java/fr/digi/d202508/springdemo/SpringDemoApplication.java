4792312b5e68c85e8ec1ca08576c4eb946bcd135
hagonin <goninha@outlook.fr>
2025-08-20 11:34:42 +0200

    spring demo start

---
+++ src/main/java/fr/digi/d202508/springdemo/SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package fr.digi.d202508.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
+
+}
